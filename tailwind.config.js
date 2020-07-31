463d268fd8395f8f1ea2c5a55f54c2b821fc1c0b
Filippo Fortino <filippofortino@gmail.com>
2020-07-31 20:02:41 +0200

    custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        'spin-bezier': 'spin 0.7s cubic-bezier(0, 0, 0.30, 0.70) infinite',
+      },
     },
   },
   variants: {},
