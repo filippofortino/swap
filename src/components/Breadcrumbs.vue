973e9c2406f86ee27c19168a13ec4e21341441b3
Filippo Fortino <filippofortino@gmail.com>
2020-08-03 22:34:56 +0200

    wip: breadcrumbs

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -2,7 +2,10 @@
   <!-- <div class="py-4 md:max-w-2xl lg:max-w-4xl xl:max-w-5xl"> -->
   <div class="py-4 w-5/6">
     <div v-if="breadcrumbs">
+      <!-- {{ formattedBreadcrumbs }} -->
+      <!-- If not in "Home" -->
       <div class="flex items-center space-x-1 md:hidden">
+        <!-- Show only back button on mobile -->
         <svg
           class="h-4 w-4 text-gray-400"
           fill="none"
@@ -23,10 +26,12 @@
       </div>
 
       <div class="hidden md:flex items-center">
-        <div class="flex items-center space-x-1 flex-shrink">
+        <!-- Show full breadcrumbs on desktop -->
+        <div class="flex items-center flex-shrink">
           <router-link
             to="/"
             class="px-2 py-1 rounded leading-tight tracking-wide text-gray-500 font-semibold hover:bg-gray-200"
+            title="Home"
           >
             Home
           </router-link>
@@ -43,18 +48,41 @@
           </svg>
         </div>
         <div
-          class="flex items-center space-x-1 overflow-auto"
-          v-for="(folder, index) in breadcrumbs.slice(1)"
+          class="flex items-center overflow-auto"
+          :class="{ 'flex-shrink-0': Array.isArray(folder) }"
+          v-for="(folder, index) in formattedBreadcrumbs"
           :key="folder.id"
         >
+          <!-- TODO: Rended a dropdown when breadcrumbs are collapsed -->
+          <span
+            v-if="Array.isArray(folder)"
+            :to="`/folder/${folder.uuid}`"
+            class="px-2 py-1 truncate rounded leading-tight tracking-wide text-gray-500 font-semibold hover:bg-gray-200"
+          >
+            <svg
+              class="h-5 w-5 text-gray-500"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>
+            </svg>
+          </span>
+
           <router-link
-            v-if="index !== breadcrumbs.slice(1).length - 1"
+            v-else-if="!isLastItemOfArray(index, formattedBreadcrumbs)"
             :to="`/folder/${folder.uuid}`"
             class="px-2 py-1 truncate rounded leading-tight tracking-wide text-gray-500 font-semibold hover:bg-gray-200"
             :title="folder.name"
           >
             {{ folder.name }}
           </router-link>
+          <!-- Last element should not be a link -->
           <span
             v-else
             class="px-2 py-1 truncate rounded leading-tight tracking-wide text-gray-500 font-semibold"
@@ -62,8 +90,10 @@
           >
             {{ folder.name }}
           </span>
+
+          <!-- Do not show arrow if last item -->
           <svg
-            v-if="index !== breadcrumbs.slice(1).length - 1"
+            v-if="!isLastItemOfArray(index, formattedBreadcrumbs)"
             class="h-4 w-4 flex-shrink-0 text-gray-400"
             fill="none"
             stroke-linecap="round"
@@ -78,7 +108,10 @@
       </div>
     </div>
     <div v-else class="flex">
-      <span class="px-2 py-1 rounded leading-tight tracking-wide text-gray-500 font-semibold">
+      <span
+        class="px-2 py-1 rounded leading-tight tracking-wide text-gray-500 font-semibold"
+        title="Home"
+      >
         Home
       </span>
     </div>
@@ -95,10 +128,28 @@ export default {
     previousFolder() {
       return this.breadcrumbs.slice(this.breadcrumbs.length - 2, this.breadcrumbs.length - 1)[0]
     },
+    formattedBreadcrumbs() {
+      const sliced = this.breadcrumbs.slice(1)
+      /**
+       * If items in breadcrumbs are five or more
+       * collapse the breadcrumbs and create an array
+       * of the middle folders to be displayed as a
+       * dropdown.
+       */
+      if (this.breadcrumbs.length >= 5) {
+        let dropdown = sliced.slice(0, sliced.length - 2)
+        return [dropdown, sliced[sliced.length - 2], sliced[sliced.length - 1]]
+      }
+
+      return sliced
+    },
   },
   methods: {
     getPreviousFolderPath() {
       return this.previousFolder.id === 1 ? '/' : `/folder/${this.previousFolder.uuid}`
+    },
+    isLastItemOfArray(index, array) {
+      return index === array.length - 1
     },
   },
 }
