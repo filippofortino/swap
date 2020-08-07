6663fabae394c64a8a35166eb833425cb841a22d
Filippo Fortino <filippofortino@gmail.com>
2020-08-07 19:29:37 +0200

    wip: new item panel animation

--- src/components/new-item/NewItemPanel.vue
+++ src/components/new-item/NewItemPanel.vue
@@ -10,7 +10,7 @@
     <div class="fixed bottom-0 right-0 w-full md:w-96 md:bottom-8 md:right-8">
       <div class="rounded-t-lg shadow-lg md:rounded-lg">
         <div class="rounded-t-lg shadow-xs bg-white md:rounded-lg">
-          <div class="pt-6 pb-8 px-5 space-y-6">
+          <div ref="parent" class="pt-6 pb-8 px-5 space-y-6">
             <div class="flex justify-between items-center">
               <div class="flex items-center space-x-2">
                 <button
@@ -38,7 +38,7 @@
               <button
                 @click="$emit('close-panel')"
                 type="button"
-                class="hidden md:inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                class="invisible md:visible inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
               >
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path
@@ -51,9 +51,25 @@
               </button>
             </div>
             <div class="space-y-8">
-              <NewItemUploadForm v-if="action === 'upload'" />
+              <transition
+                mode="out-in"
+                enter-active-class="transition ease-out duration-200"
+                enter-class="transform opacity-0 scale-95"
+                enter-to-class="transform opacity-100 scale-100"
+                leave-active-class="transition ease-in duration-200"
+                leave-class="transform opacity-100 scale-100"
+                leave-to-class="transform opacity-0 scale-95"
+              >
+                <component
+                  ref="nonloso"
+                  :is="actionToComponent"
+                  @select-action="action = $event"
+                  v-on="$listeners"
+                />
+              </transition>
+              <!-- <NewItemUploadForm v-if="action === 'upload'" />
               <NewItemCreateFolderForm v-else-if="action === 'new-folder'" v-on="$listeners" />
-              <NewItemSelectAction v-else @select-action="action = $event" />
+              <NewItemSelectAction v-else @select-action="action = $event" /> -->
             </div>
           </div>
         </div>
@@ -67,8 +83,11 @@ import NewItemSelectAction from './NewItemSelectAction.vue'
 import NewItemUploadForm from './NewItemUploadForm'
 import NewItemCreateFolderForm from './NewItemCreateFolderForm'
 
+import smoothReflow from 'vue-smooth-reflow'
+
 export default {
   name: 'NewItemPanel',
+  mixins: [smoothReflow],
   data() {
     return {
       action: null,
@@ -79,6 +98,30 @@ export default {
     NewItemUploadForm,
     NewItemCreateFolderForm,
   },
-  computed: {},
+  computed: {
+    actionToComponent() {
+      switch (this.action) {
+        case 'upload':
+          return 'NewItemUploadForm'
+
+        case 'new-folder':
+          return 'NewItemCreateFolderForm'
+
+        case 'select':
+          return 'NewItemSelectAction'
+
+        default:
+          return 'NewItemSelectAction'
+      }
+    },
+  },
+  mounted() {
+    this.$smoothReflow({
+      el: this.$refs.parent,
+      transitionEvent: {
+        el: this.$refs.nonloso,
+      },
+    })
+  },
 }
 </script>
