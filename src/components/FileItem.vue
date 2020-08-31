292f4add22931ed20b7c334098b67da497f78b25
Filippo Fortino <filippofortino@gmail.com>
2020-08-31 20:17:14 +0200

    wip: make items selectable
    Removed dropdown but allow users to perform actions on files
    by selcting them

--- src/components/FileItem.vue
+++ src/components/FileItem.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="rounded-md shadow-sm">
-    <button
-      type="button"
-      class="group w-full rounded-md shadow-sm border border-gray-300 focus:outline-none focus:shadow-outline-indigo"
-      :title="file.name"
-    >
-      <!-- If file have a preview image -->
-      <div class="overflow-hidden rounded-t-md">
+  <div
+    class="group rounded-lg shadow-sm border-2 box-content"
+    :class="{ ' border-indigo-600': isSelected, 'border-transparent': !isSelected }"
+  >
+    <div class="relative border rounded-md">
+      <button
+        type="button"
+        class="block w-full h-full overflow-hidden rounded-md shadow focus:outline-none focus:shadow-outline-indigo"
+        :title="file.name"
+      >
+        <!-- If file have a preview image -->
         <img
           v-if="file.preview"
           class="h-28 w-full object-cover sm:h-32 md:h-36"
@@ -32,68 +35,59 @@
             ></path>
           </svg>
         </div>
-      </div>
-      <div class="relative">
-        <div class="p-3 rounded-b-md bg-white truncate sm:p-4">
+        <div class="p-3 bg-white truncate sm:p-4">
           <span class="text-xs leading-tight font-semibold text-gray-600 sm:text-sm">
             {{ file.name }}
           </span>
         </div>
-        <div
-          class="absolute flex items-center top-0 right-0 h-full opacity-0 z-10 group-hover:opacity-100 focus-within:opacity-100 transition-opacity duration-200"
-        >
-          <Dropdown>
-            <button
-              slot="trigger"
-              slot-scope="{ open }"
-              type="button"
-              id="options-menu"
-              aria-haspopup="true"
-              :aria-expanded="open"
-              class="px-0.5 py-2 mr-0.5 truncate rounded leading-tight tracking-wide text-gray-500 bg-white font-semibold hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
-            >
-              <svg
-                class="h-6 w-6 text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-                />
-              </svg>
-            </button>
+      </button>
 
-            <DropdownItem to="#" title="title" class="flex items-center space-x-3">
-              Item 1
-            </DropdownItem>
-            <DropdownItem to="#" title="title" class="flex items-center space-x-3">
-              Item 2
-            </DropdownItem>
-            <DropdownItem to="#" title="title" class="flex items-center space-x-3">
-              Item 3
-            </DropdownItem>
-          </Dropdown>
-        </div>
+      <div
+        class="absolute flex justify-end top-0 right-0 w-full rounded-t-md bg-gradient-to-b from-gray-500 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+        :class="{ 'opacity-100': isSelected }"
+      >
+        <button
+          @click="toggleSelected()"
+          type="button"
+          class="mt-2 mr-2 mb-4 transition-colors duration-200 focus:outline-none"
+          :class="{
+            'text-indigo-600': isSelected,
+            'text-gray-200 hover:text-white focus:text-white': !isSelected,
+          }"
+        >
+          <svg
+            class="h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </button>
       </div>
-    </button>
+    </div>
   </div>
 </template>
 
 <script>
-import Dropdown from '@/components/dropdown/Dropdown'
-import DropdownItem from '@/components/dropdown/DropdownItem'
-
 export default {
   name: 'FileItem',
-  components: {
-    Dropdown,
-    DropdownItem,
-  },
   props: ['file'],
+  data() {
+    return {
+      isSelected: false,
+    }
+  },
+  methods: {
+    toggleSelected() {
+      this.isSelected = !this.isSelected
+    },
+  },
 }
 </script>
--- src/components/FolderItem.vue
+++ src/components/FolderItem.vue
@@ -1,40 +1,46 @@
 <template>
-  <div class="relative group rounded-md shadow-sm">
-    <router-link
-      :to="`/folder/${folder.uuid}`"
-      class="block rounded-md shadow-sm border border-gray-300 overflow-hidden focus:outline-none focus:shadow-outline-indigo select-none"
-      :title="folder.name"
-    >
-      <div class="p-3 bg-white sm:p-4">
-        <div class="flex items-center space-x-2">
-          <svg
-            class="h-6 w-6 flex-shrink-0 text-gray-700 sm:h-7 sm:w-7"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path>
-          </svg>
-          <span class="text-xs font-semibold text-gray-600 truncate sm:text-sm">
-            {{ folder.name }}
-          </span>
+  <div
+    class="group rounded-lg shadow-sm border-2 bg-white"
+    :class="{ ' border-indigo-600': isSelected, 'border-transparent': !isSelected }"
+  >
+    <div class="relative border rounded-md">
+      <router-link
+        :to="`/folder/${folder.uuid}`"
+        class="block w-full h-full rounded-md shadow focus:outline-none focus:shadow-outline-indigo select-none"
+        :title="folder.name"
+      >
+        <div class="p-3 sm:p-4">
+          <div class="flex items-center space-x-2">
+            <svg
+              class="h-6 w-6 flex-shrink-0 text-gray-700 sm:h-7 sm:w-7"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+              ></path>
+            </svg>
+            <span class="text-xs font-semibold text-gray-600 truncate sm:text-sm">
+              {{ folder.name }}
+            </span>
+          </div>
         </div>
-      </div>
-    </router-link>
-    <div
-      class="absolute flex items-center top-0 right-0 h-full opacity-0 z-10 group-hover:opacity-100 focus-within:opacity-100 transition-opacity duration-200"
-    >
-      <Dropdown>
+      </router-link>
+      <div
+        class="absolute flex items-center top-0 right-0 h-full rounded-r-md bg-gradient-to-r from-transparent to-gray-200 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+        :class="{ 'opacity-100': isSelected }"
+      >
         <button
-          slot="trigger"
-          slot-scope="{ open }"
+          @click="toggleSelected()"
           type="button"
-          id="options-menu"
-          aria-haspopup="true"
-          :aria-expanded="open"
-          class="px-0.5 py-2 mr-0.5 truncate rounded leading-tight tracking-wide text-gray-500 bg-white font-semibold hover:bg-gray-200 focus:outline-none focus:bg-gray-200"
+          class="mr-1 ml-3 transition-colors duration-200 focus:outline-none"
+          :class="{
+            'text-indigo-600': isSelected,
+            'text-gray-400 hover:text-gray-500 focus:text-gray-500': !isSelected,
+          }"
         >
           <svg
-            class="h-6 w-6 text-gray-500"
+            class="h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -44,35 +50,28 @@
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
         </button>
-
-        <DropdownItem to="#" title="title" class="flex items-center space-x-3">
-          Item 1
-        </DropdownItem>
-        <DropdownItem to="#" title="title" class="flex items-center space-x-3">
-          Item 2
-        </DropdownItem>
-        <DropdownItem to="#" title="title" class="flex items-center space-x-3">
-          Item 3
-        </DropdownItem>
-      </Dropdown>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Dropdown from '@/components/dropdown/Dropdown'
-import DropdownItem from '@/components/dropdown/DropdownItem'
-
 export default {
   name: 'FolderItem',
-  components: {
-    Dropdown,
-    DropdownItem,
-  },
   props: ['folder'],
+  data() {
+    return {
+      isSelected: false,
+    }
+  },
+  methods: {
+    toggleSelected() {
+      this.isSelected = !this.isSelected
+    },
+  },
 }
 </script>
