67da8d4b5f723d4264be76d0acf95a19032689d0
Filippo Fortino <filippofortino@gmail.com>
2020-09-08 21:47:02 +0200

    wip: fix z-index issue
    New Item Panel was displaying over modal overlay

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed z-20 inset-0 overflow-y-auto">
+  <div class="fixed z-30 inset-0 overflow-y-auto">
     <div
       class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
     >
