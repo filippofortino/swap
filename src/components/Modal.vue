849771707cbf5c910410d80e22a9438fb22c5761
Filippo Fortino <filippofortino@gmail.com>
2020-09-08 19:03:34 +0200

    wip: button state on submit

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -104,23 +104,21 @@
             </div>
           </div>
           <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-              <button
-                type="submit"
+            <span class="sm:ml-3">
+              <Button
+                type="blue"
+                nativeType="submit"
                 form="rename-form"
-                class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+                :disabled="isButtonDisabled"
+                :loading="loading"
               >
                 Rename
-              </button>
+              </Button>
             </span>
-            <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
-              <button
-                type="button"
-                class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
-                @click="$emit('close')"
-              >
+            <span class="mt-3 sm:mt-0">
+              <Button type="white" @click="$emit('close')">
                 Cancel
-              </button>
+              </Button>
             </span>
           </div>
         </div>
@@ -132,6 +130,7 @@
 <script>
 import axios from 'axios'
 import Input from './Input'
+import Button from './Button'
 
 export default {
   name: 'Modal',
@@ -141,12 +140,15 @@ export default {
       newItemName: this.item.item.name,
       errorMessage: false,
       genericErrorHappened: false,
+      loading: false,
     }
   },
   methods: {
     async rename() {
       const { item, type } = this.item
       try {
+        this.loading = true
+
         await axios.patch(`https://api.swap.test/${type}/${item.id}`, {
           folder_id: item.folder_id,
           name: this.newItemName,
@@ -162,11 +164,19 @@ export default {
       } catch (e) {
         if (e.response.status === 422) this.errorMessage = e.response.data.errors.name[0]
         else this.genericErrorHappened = true
+      } finally {
+        this.loading = false
       }
+    },
+  },
+  computed: {
+    isButtonDisabled() {
+      return this.newItemName < 1
     },
   },
   components: {
     Input,
+    Button,
   },
 }
 </script>
